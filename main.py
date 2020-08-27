a91381737c1893a7cb0dbbae090bb042fd438c6f
Lucien Shui <lucien@lucien.ink>
2020-08-27 11:43:07 +0800

    Add arg parser

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from __future__ import absolute_import, print_function
 
-import re
-import sys
+import argparse
+import logging
 import queue
 import time
 
 from agent import DeepQNetwork
 from graphic import CursesSnake
 from gym.envs import SnakeEnv
+
+logging.basicConfig(level=logging.INFO)
 
 
 def distance_reward(info: dict) -> float:
@@ -47,83 +49,93 @@ def agent_play(shape: tuple, render: bool = False):
     env.close()
 
 
-def train(shape: tuple, render: bool = False):
+def train_step(shape: tuple, env: SnakeEnv, dqn: DeepQNetwork, render: bool = False) -> int:
+    observation = env.reset()
+    action_cnt: int = 0
+    action_queue: queue.Queue = queue.Queue()
+    for t in range(1 << 11):
+        if render:
+            env.render()
+        action = dqn.greedy_action(observation)
+
+        next_observation, reward, done, info = env.step(action)
+        action_cnt = 0 if reward > 0 else action_cnt + 1
+
+        def custom_reward() -> (float, done):
+
+            # 如果原地转圈则惩罚
+            action_queue.put(action)
+            while action_queue.qsize() > 4:
+                action_queue.get()
+
+            if action_queue.qsize() == 4 and set(action_queue.queue).__len__() == 1 and action_queue.queue[0] != 0:
+                return -1, False
+
+            # 如果无用步数过多则提前结束并惩罚
+            if action_cnt > shape[0] * shape[1] * 2:
+                return -1, False
+
+            return reward, done
+
+        reward, done = custom_reward()
+
+        # 记录并学习
+        dqn.fit(observation, reward, done, action, next_observation)
+
+        observation = next_observation
+        if done:
+            break
+    return env.curses_snake.snake.length
+
+
+def train(shape: tuple, render: bool = False, episode: int = 2048):
     env = SnakeEnv(shape)
 
-    dqn = DeepQNetwork(shape, len(env.action_space))
+    dqn = DeepQNetwork(shape, len(env.action_space), initial_epsilon=2)
 
     best_score: int = 0
 
-    for i_episode in range(2000):
-        observation = env.reset()
-        action_cnt: int = 0
-        action_queue: queue.Queue = queue.Queue()
-        for t in range(1000):
-            if render:
-                env.render()
-            action = dqn.greedy_action(observation)
+    i_episode = 1
 
-            next_observation, reward, done, info = env.step(action)
-            action_cnt = 0 if reward > 0 else action_cnt + 1
+    while True:
+        best_score = max(best_score, train_step(shape, env, dqn, render=render))
 
-            def custom_reward() -> (float, done):
+        if i_episode % 100 == 0:
+            dqn.save('model.h5')
+            print(f"Episode {i_episode} finished, best score is {best_score}")
 
-                # 如果原地转圈则惩罚
-                action_queue.put(action)
-                while action_queue.qsize() > 4:
-                    action_queue.get()
+        if episode is not None and i_episode > episode:
+            break
 
-                if action_queue.qsize() == 4 and set(action_queue.queue).__len__() == 1 and action_queue.queue[0] != 0:
-                    return -1, False
-
-                # 如果无用步数过多则提前结束并惩罚
-                if action_cnt > shape[0] * shape[1] * 2:
-                    return -1, False
-
-                return reward, done
-
-            reward, done = custom_reward()
-
-            # 记录并学习
-            dqn.fit(observation, reward, done, action, next_observation)
-
-            observation = next_observation
-            if done:
-                best_score = max(best_score, env.curses_snake.snake.length)
-                if (i_episode + 1) % 100 == 0:
-                    dqn.save('model.h5')
-                    print("Episode {} finished after {} time steps, best score is {}".format(
-                        i_episode + 1, t + 1, best_score))
-                break
     env.close()
 
     dqn.save('model.h5')
 
 
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--human', help='human play', action='store_true')
+    parser.add_argument('--training', help='training agent', action='store_true')
+    parser.add_argument('--render', help='render game', action='store_true')
+    parser.add_argument('--shape', help='game size', nargs=2, type=int)
+    parser.add_argument('--episode', help='training episode', type=int)
+
+    args = parser.parse_args()
+
+    args.shape = (4, 4) if args.shape is None else tuple(args.shape)
+
+    return args
+
+
 def main():
-    config = {
-        'shape': '(4, 4)',
-        'human': 'false',
-        'render': 'false',
-        'training': 'false'
-    }
+    args = get_args()
 
-    if len(sys.argv) > 1:
-        for i in range(1, len(sys.argv)):
-            key, value = sys.argv[i].split('=')
-            config[key[2:]] = value
-
-    shape: tuple = tuple(map(int, re.findall(r'\d+', config['shape'])))
-    human: bool = config['human'][0] in ['t', '1']
-    render: bool = config['render'][0] in ['t', '1']
-    training: bool = config['training'][0] in ['t', '1']
-
-    if human:
-        CursesSnake(shape).run()
-    elif training:
-        train(shape, render=render)
+    if args.human:
+        CursesSnake(args.shape).run()
+    elif args.training:
+        train(args.shape, render=args.render, episode=args.episode)
     else:
-        agent_play(shape, render=render)
+        agent_play(args.shape, render=args.render)
 
 
 if __name__ == '__main__':
